70c521c2115fd5a801b6e1802f9c5217daeda487
Mnjaro <grt.aria@gmail.com>
2021-03-11 10:12:14 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/main.less';
+
+const Welcome = () => <h1>Hello World from React boilerplate</h1>;
+
+ReactDOM.render(<Welcome />, document.getElementById('root'));
